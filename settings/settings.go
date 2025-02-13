cb68a17a3ff30ac11cc179d9c03f4f2d087a9940
Ken288yzs1 <cess1929@outlook.com>
2025-02-13 23:18:42 +0800

    Add files via upload

--- settings/settings.go
+++ settings/settings.go
@@ -1,1 +1,36 @@
+package settings
 
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+)
+
+type RayConfig struct {
+	ClientID      string   `env:"CLIENT_ID"`
+	ClientSecret  string   `env:"CLIENT_SECRET"`
+	Email         string   `env:"EMAIL"`
+	Password      string   `env:"PASSWORD"`
+	Token         string   `env:"TOKEN" env-default:""`
+	ExternalToken []string `env:"EXTERNAL_TOKEN" env-default:""`
+	Port          int      `env:"PORT" env-default:"7860"`
+}
+
+var rayConf RayConfig
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		logrus.WithError(err).Warn("load .env file error, try to read from env")
+	}
+	err := cleanenv.ReadEnv(&rayConf)
+	if err != nil {
+		logrus.Panic("read env error", err)
+	}
+	if len(rayConf.ExternalToken) == 0 {
+		logrus.Warn("ExternalToken is empty, skip auth, recommend to set it")
+	}
+}
+
+func Get() RayConfig {
+	return rayConf
+}
