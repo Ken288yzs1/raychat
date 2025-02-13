d3473ddf5df1bf58745a6116c9df431d75846eed
Ken288yzs1 <cess1929@outlook.com>
2025-02-13 23:17:22 +0800

    Add files via upload

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -1,1 +1,29 @@
+package middlewares
 
+import (
+	"raychat/settings"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/samber/lo"
+)
+
+func Auth(c *gin.Context) {
+	if len(settings.Get().ExternalToken) == 0 {
+		c.Next()
+		return
+	}
+
+	rawtoken := c.GetHeader("Authorization")
+	tokenStrlist := strings.Split(rawtoken, " ")
+	if len(tokenStrlist) != 2 || len(rawtoken) == 0 {
+		c.AbortWithStatusJSON(401, gin.H{"message": "Unauthorized"})
+		return
+	}
+	token := tokenStrlist[1]
+	if !lo.Contains(settings.Get().ExternalToken, token) {
+		c.AbortWithStatusJSON(401, gin.H{"message": "Unauthorized"})
+		return
+	}
+	c.Next()
+}
